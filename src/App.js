0c8e07e25f3dabda4b9981536d343de73a59f3d0
vrajkmrpatel <82221992+vrajkmrpatel@users.noreply.github.com>
2021-11-29 16:19:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-// import logo from "./logo.svg";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
-import Footer from "./components/Footer";
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react';
 
-
+ 
 function App() {
-  // const [text, setText] = useState('Enter text here');
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-
-    <Navbar title="TextConverter"/>
-      <div className="container my-3">
-          <TextForm heading ="Enter the Text to analyze"/>
-      </div>
-      <div className="container my-3">
-        <Footer/>
-      </div>
-    </>
+    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+    {/* <Navbar/> */}
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <div className="container my-3">
+    <TextForm heading="Enter the text to analyze below" mode={mode}/>
+    </div>
+    </> 
   );
 }
 
